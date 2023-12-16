4d0121299c3f14d279bacde748b71ac6ded416ba
atemmel <temmeladam@gmail.com>
2023-12-16 01:54:23 +0100

    Added base for sorts

--- atemmel/dumb.hpp
+++ atemmel/dumb.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <algorithm>
+
 template <typename T>
 auto dumbSort(T *first, T *last) -> void {
-    for (auto it = first; it != last; it++) {
-        for (auto jt = first; jt != last; jt++) {
-            if (*it < *jt) {
-                std::iter_swap(it, jt);
-            }
-        }
-    }
+    std::sort(first, last);
 }
+
+struct DumbSort {
+    template <typename T>
+    auto operator()(T *first, T *last) -> void {
+        dumbSort(first, last);
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,27 +1,107 @@
-#include <algorithm>
+#include <cxxabi.h>
+
+#include <chrono>
+#include <cstdint>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <string_view>
+#include <typeinfo>
 #include <vector>
 
 #include "atemmel/dumb.hpp"
 
-auto ensure(bool condition, std::string_view msg) -> void {
+struct Datasets {
+    std::vector<std::string> stringDataset;
+    std::vector<int64_t> integerDataset;
+};
+
+auto eprint(std::string_view msg) -> void {
+    std::cerr << msg << '\n';
+};
+
+auto eprint(std::string_view msg, std::string_view rest...) -> void {
+    std::cerr << msg;
+    eprint(rest);
+};
+
+auto ensure(bool condition, std::string_view msg...) -> void {
     if (!condition) {
         std::cout << msg << '\n';
         std::exit(1);
     }
 }
 
+auto readStrings() -> std::vector<std::string> {
+    auto strings = std::vector<std::string>();
+    auto file    = std::ifstream("datasets/string-dataset.txt");
+    std::string line;
+    while (std::getline(file, line)) {
+        strings.push_back(line);
+    }
+    return strings;
+}
+
+auto readInts() -> std::vector<int64_t> {
+    auto ints = std::vector<int64_t>();
+    auto file = std::ifstream("datasets/integer-dataset.txt");
+    std::string line;
+    while (std::getline(file, line)) {
+        ints.push_back(std::stoi(line));
+    }
+    return ints;
+}
+
+template <typename T>
+auto prettyTypeid() -> std::string {
+    int status;
+    char* demangled = abi::__cxa_demangle(typeid(T).name(), 0, 0, &status);
+    std::string str = demangled;
+    free(demangled);
+    return str;
+}
+
+template <typename SortImpl, typename Dataset>
+auto benchmarkImpl(SortImpl sort, Dataset dataset, std::string_view datasetName)
+    -> void {
+    auto isSorted = [](const auto& v) -> bool {
+        return std::is_sorted(v.begin(), v.end());
+    };
+
+    auto now = []() -> double {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(
+                   std::chrono::high_resolution_clock::now().time_since_epoch())
+            .count();
+    };
+
+    {
+        std::cout << datasetName << ": ";
+        std::cout.flush();
+
+        auto before = now();
+        sort(&*dataset.begin(), &*dataset.end());
+        auto after = now();
+
+        ensure(isSorted(dataset), prettyTypeid<SortImpl>(),
+               " did not manage to sort ", datasetName);
+        auto dt = after - before;
+        std::cout << dt << "ms\n";
+    }
+}
+
+template <typename SortImpl>
+auto benchmark(SortImpl sort, const Datasets& datasets) -> void {
+    std::cerr << "Benchmarking " << prettyTypeid<SortImpl>() << ":\n";
+    benchmarkImpl(sort, datasets.stringDataset, "string-dataset");
+    benchmarkImpl(sort, datasets.integerDataset, "integer-dataset");
+}
+
 auto main() -> int {
-    std::vector<int> x = {
-        1, 5, 3, 7, 2,
+    const auto datasets = Datasets{
+        .stringDataset  = readStrings(),
+        .integerDataset = readInts(),
     };
 
-    auto sortCheck = [&]() -> bool {
-        return std::is_sorted(x.begin(), x.end());
-    };
-
-    dumbSort(&*x.begin(), &*x.end());
-    ensure(sortCheck(), "dumbSort did not sort as expected");
+    benchmark(DumbSort{}, datasets);
 }
